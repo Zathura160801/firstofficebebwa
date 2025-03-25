a617cd1af9e284ccccb43e0e888ed988abe08faf
Zathura160801 <adipz160801@gmail.com>
2025-03-26 01:35:39 +0800

    mass assignment, orm relationship

--- app/Models/OfficeSpaceBenefit.php
+++ app/Models/OfficeSpaceBenefit.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class OfficeSpaceBenefit extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+    
+    protected $fillable = [
+        'name',
+        'office_space_id',
+    ]
 }
--- app/Models/OfficeSpacePhoto.php
+++ app/Models/OfficeSpacePhoto.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class OfficeSpacePhoto extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+    
+    protected $fillable = [
+        'photo',
+        'office_space_id',
+    ];
 }
